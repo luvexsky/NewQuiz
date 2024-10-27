5d9cf8bfbfe11e62dbe12638976195469f5d7e1b
luvexsky <ngryemo@gmail.com>
2024-10-27 18:45:35 -0400

    Used an array to store answer options and get the correct answer index.

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -8,9 +8,31 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] private string question = "Enter new question here";
+    
+    // Array to store possible answers
+    [SerializeField] string[] answers = new string[4];
+
+    // Index to mark the correct answer
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
     {
         return question;
     }
-}
+
+    public string[] GetAnswers()
+    {
+        return answers;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+      return answers[index];
+        }
+       
+    }
