0745111c53e861c698771b317ba7e0aadf18b53c
luvexsky <ngryemo@gmail.com>
2024-10-27 23:46:21 -0400

    Calculated and implemented fillFraction for the timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,12 +6,19 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
     float timerValue;
 
     void Update()
     {
         UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
     }
 
     void UpdateTimer()
@@ -20,7 +27,11 @@ public class Timer : MonoBehaviour
 
         if (isAnsweringQuestion)
         {
-            if (timerValue <= 0)
+            if(timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion; 
+            }
+            else
             {
                 isAnsweringQuestion = false;
                 timerValue = timeToShowCorrectAnswer;
@@ -28,11 +39,14 @@ public class Timer : MonoBehaviour
         }
         else
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
             {
+                 fillFraction = timerValue / timeToShowCorrectAnswer; 
+            }
+            else
                 isAnsweringQuestion = true;
                 timerValue = timeToCompleteQuestion;
+                loadNextQuestion= true;
             }
         }
     }
-}
